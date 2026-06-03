4f513252fb8af882f167066a0e8fe6f6d73bc107
huyhuy <23.1015.huydinhcong@gmail.com>
2026-06-04 03:12:08 +0700

    comment đầu tiên

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const route = require("./routes/client/index.route");
+
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+route(app);
+
+// app.get("/", (req, res) => {
+//     res.render("client/pages/home/index");
+// });
+
+// app.get("/products", (req, res) => {
+//     res.render("client/pages/products/index");
+// });
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
